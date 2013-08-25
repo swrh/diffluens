5e11072fc663ff36c58607580695cf26674078b9
Fernando Silveira <fsilveira@gmail.com>
2013-08-25 12:15:26 -0300

    Started "timesheet" app.

--- diffluens/settings.py
+++ diffluens/settings.py
@@ -130,6 +130,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'timesheet',
 )
 
 # A sample logging configuration. The only tangible logging
