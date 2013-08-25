2d61b3788ab09f0866feb60b683546bfa0784a8b
Fernando Silveira <fsilveira@gmail.com>
2013-08-25 18:48:36 -0300

    Created "timesheet.Event" entity.

--- timesheet/models.py
+++ timesheet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    issue = models.IntegerField()
+    begin = models.DateTimeField()
+    end = models.DateTimeField(null = True, blank = True)
+    all_day = models.BooleanField()
+    user = models.ForeignKey(User)
+
+# vi:set sw=4 ts=4:
+# vim:set et:
