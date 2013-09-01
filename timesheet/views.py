0a3a5a8ccaa8b635cd31942c9fdf587b258ad951
Fernando Silveira <fsilveira@gmail.com>
2013-09-01 14:48:36 -0300

    Disabling timezone support.

--- timesheet/views.py
+++ timesheet/views.py
@@ -1,9 +1,9 @@
 import json
+import time
 
 from datetime import datetime
 
 from django.http import HttpResponse
-from django.utils.timezone import utc
 from django.core.exceptions import PermissionDenied
 
 from timesheet.models import Event
@@ -18,16 +18,16 @@ def events_read(request):
         params = request.GET
     if params == None or params.get('start') == None or params.get('end') == None:
         raise PermissionDenied
-    begin = datetime.fromtimestamp(int(params['start']), utc)
-    end = datetime.fromtimestamp(int(params['end']), utc)
+    begin = datetime.fromtimestamp(int(params['start']))
+    end = datetime.fromtimestamp(int(params['end']))
     evs = Event.objects.filter(user = request.user, begin__gte = begin, begin__lte = end)
     output = []
     for ev in evs:
         e = {}
         e['title'] = unicode(ev.issue)
-        e['start'] = ev.begin.strftime('%Y-%m-%d %H:%M')
+        e['start'] = int(time.mktime(ev.begin.timetuple()))
         if ev.end != None:
-            e['end'] = ev.end.strftime('%Y-%m-%d %H:%M')
+            e['end'] = int(time.mktime(ev.end.timetuple()))
         e['allDay'] = ev.all_day
         e['id'] = ev.id
         output.append(e)
