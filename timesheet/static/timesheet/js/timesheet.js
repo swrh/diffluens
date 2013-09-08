8d37da5fab41e1710ac417696efb9e9b99417d55
Fernando Silveira <fsilveira@gmail.com>
2013-09-08 14:03:47 -0300

    Implementing events reading in calendar.

--- timesheet/static/timesheet/js/timesheet.js
+++ timesheet/static/timesheet/js/timesheet.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-  var date = new Date();
-  var d = date.getDate();
-  var m = date.getMonth();
-  var y = date.getFullYear();
-
   $('#calendar').fullCalendar({
     header: {
       left: 'prev,next today',
@@ -11,52 +6,33 @@ $(document).ready(function() {
       right: 'month,agendaWeek,agendaDay',
     },
     editable: true,
-    events: [
-      {
-        title: 'All Day Event',
-        start: new Date(y, m, 1),
-      },
-      {
-        title: 'Long Event',
-        start: new Date(y, m, d-5),
-        end: new Date(y, m, d-2),
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d-3, 16, 0),
-        allDay: false,
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d+4, 16, 0),
-        allDay: false,
-      },
-      {
-        title: 'Meeting',
-        start: new Date(y, m, d, 10, 30),
-        allDay: false,
-      },
-      {
-        title: 'Lunch',
-        start: new Date(y, m, d, 12, 0),
-        end: new Date(y, m, d, 14, 0),
-        allDay: false,
-      },
-      {
-        title: 'Birthday Party',
-        start: new Date(y, m, d+1, 19, 0),
-        end: new Date(y, m, d+1, 22, 30),
-        allDay: false,
-      },
-      {
-        title: 'Click for Google',
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        url: 'http://google.com/',
-      },
-    ],
+    events: function(start, end, callback) {
+      $.ajax({
+        url: '/timesheet/events/read/',
+        type: 'GET',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: {
+          begin: Math.round(start.getTime() / 1000),
+          end: Math.round(end.getTime() / 1000),
+        },
+        success: function(data) {
+          for (var i in data) {
+            var d = data[i];
+            var e = {};
+            e.allDay = d.all_day;
+            e.start = d.begin;
+            e.end = d.end;
+            e.title = '' + d.issue;
+            data[i] = e;
+          }
+          callback(data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+          alert('Failure while fetching events!');
+        },
+      });
+    },
   });
 });
 
