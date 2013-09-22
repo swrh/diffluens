077a3577376dd54a3946e4a6d7e2183b147933b2
Fernando Silveira <fsilveira@gmail.com>
2013-09-22 14:39:15 -0300

    Implemented event creation in the client side.

--- timesheet/static/timesheet/js/timesheet.js
+++ timesheet/static/timesheet/js/timesheet.js
@@ -7,6 +7,17 @@
     return str.substr(str.length - len);
   }
 
+  Date.prototype.ddmmyyyy = function() {
+    return numberToString(this.getDate(), 2) + '/' +
+      numberToString(this.getMonth() + 1, 2) + '/' +
+      numberToString(this.getFullYear(), 4);
+  }
+
+  Date.prototype.hhmm = function() {
+    return numberToString(this.getHours(), 2) + ':' +
+      numberToString(this.getMinutes(), 2);
+  }
+
   Date.prototype.toISO = function() {
     return numberToString(this.getFullYear(), 4) + '-' +
       numberToString(this.getMonth() + 1, 2) + '-' +
@@ -14,6 +25,11 @@
       numberToString(this.getHours(), 2) + ':' +
       numberToString(this.getMinutes(), 2) + ':' +
       numberToString(this.getSeconds(), 2);
+  }
+
+  Date.prototype.offsetTZ = function() {
+    this.setTime(this.getTime() + (this.getTimezoneOffset() * 60 * 1000));
+    return this;
   }
 
   /*
@@ -115,6 +131,160 @@
   });
 })();
 
+/*
+ * dialogIssue plugin.
+ */
+(function($) {
+  var defaults = {
+  };
+
+  $.fn.dialogIssue = function(options) {
+    if (typeof(options) == 'string') {
+      var args = Array.prototype.slice.call(arguments, 1);
+      var res;
+      this.each(function() {
+        var de = $.data(this, 'dialogIssue');
+        if (de && $.isFunction(de[options])) {
+          var r = de[options].apply(de, args);
+          if (res === undefined) {
+            res = r;
+          }
+          if (options == 'destroy') {
+            $.removeData(this, 'dialogIssue');
+          }
+        }
+      });
+      if (res !== undefined) {
+        return res;
+      }
+      return this;
+    }
+
+    options = options || {};
+
+    options = $.extend(defaults, options);
+
+    this.each(function(i, _element) {
+      var element = $(_element);
+      var de = new DialogIssue(element, options);
+      element.data('dialogIssue', de);
+    });
+
+    return this;
+  }
+
+  var DialogIssue = function(element, options) {
+    var this_ = this;
+
+    this.element = element;
+    this.options = options;
+
+    element.html(
+      '<form>\n' +
+          '<label class="label-block" for="date">Date</label>\n' +
+          '<input id="date" class="text ui-corner-all ui-widget-content input-block" type="text" disabled="true" />\n' +
+          '<label><input name="allday" id="allday" type="checkbox" disabled="true" /> All day</label>\n' +
+          '<div class="issue-time">\n' +
+          '<div class="issue-time-begin">\n' +
+          '<label class="label-block" for="begin">Begin</label>\n' +
+          '<input id="begin" class="text ui-corner-all ui-widget-content input-block" type="text" disabled="true" />\n' +
+          '</div>\n' +
+          '<div class="issue-time-end">\n' +
+          '<label class="label-block" for="end">End</label>\n' +
+          '<input id="end" class="text ui-corner-all ui-widget-content input-block" type="text" disabled="true" />\n' +
+          '</div>\n' +
+          '</div>\n' +
+          '<label class="label-block" for="issue">Issue</label>\n' +
+          '<input id="issue" class="text ui-corner-all ui-widget-content input-block" type="text" autofocus />\n' +
+      '</form>');
+    element.dialog({
+      minWidth: 300,
+      autoOpen: false,
+      modal: true,
+      buttons: {
+        'OK': function() {
+          this_.save();
+        },
+        'Cancel': function() {
+          this_.cancel();
+        },
+      },
+    });
+  }
+
+  DialogIssue.prototype.open = function(options) {
+    this.onClickOk = options.onClickOk;
+
+    this.event = options.event;
+
+    this.element.find('.tip').text('').removeClass('ui-state-highlight');
+    this.element.find('.text').val('').removeClass('ui-state-error');
+
+    this.element.find('#date').val(this.event.begin.ddmmyyyy());
+    if (this.event.all_day) {
+      this.element.find('.issue-time').hide();
+      this.element.find('#allday').prop('checked', true);
+    } else {
+      this.element.find('.issue-time').show();
+      this.element.find('#allday').prop('checked', false);
+      this.element.find('#begin.text').val(this.event.begin.hhmm());
+      this.element.find('#end.text').val(this.event.end.hhmm());
+    }
+    this.element.find('#issue.text').val(this.event.issue);
+
+    this.element.dialog('open');
+  }
+
+  DialogIssue.prototype.save = function() {
+    this.element.find('.tip').text('').removeClass('ui-state-highlight');
+    this.element.find('.text').removeClass('ui-state-error');
+
+    var issue = this.element.find('#issue.text').val().trim();
+    if (issue.length <= 0) {
+      this.element.find('#issue.text').addClass('ui-state-error');
+      this.element.find('#issue.tip').text('Issue must not be empty.').addClass('ui-state-highlight');
+      return;
+    }
+
+    this.event.issue = issue;
+
+    if (this.onClickOk != null) {
+      this.onClickOk(this.event);
+    } else {
+      this.close();
+    }
+  }
+
+  DialogIssue.prototype.cancel = function() {
+    this.close();
+  }
+
+  DialogIssue.prototype.close = function() {
+    this.element.dialog('close');
+  }
+})(jQuery);
+
+/*
+ * Diffluens + FullCalendar integration stuff.
+ */
+(function() {
+  jsonD2F = function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var d = data[i];
+      var e = {};
+      e.allDay = d.all_day;
+      e.start = new Date(d.begin).offsetTZ();
+      e.end = new Date(d.end).offsetTZ();
+      e.title = '' + d.issue;
+      data[i] = e;
+    }
+    return data;
+  }
+})();
+
+/*
+ * Initialization stuff.
+ */
 $(document).ready(function() {
   $('#dialog-alert').dialog({
     modal: true,
@@ -122,6 +292,8 @@ $(document).ready(function() {
     autoOpen: false,
   });
   $('#dialog-alert').html('<p style="text-align: center;"></p>');
+
+  $('#dialog-issue').dialogIssue();
 
   /*
    * Initialize FullCalendar.
@@ -149,15 +321,7 @@ $(document).ready(function() {
           end: end.toISO(),
         },
         success: function(data) {
-          for (var i in data) {
-            var d = data[i];
-            var e = {};
-            e.allDay = d.all_day;
-            e.start = d.begin;
-            e.end = d.end;
-            e.title = '' + d.issue;
-            data[i] = e;
-          }
+          jsonD2F(data);
           callback(data);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -168,6 +332,7 @@ $(document).ready(function() {
       });
     },
     selectable: true,
+    selectHelper: true,
     select: function(start, end, allDay) {
       $('#calendar').fullCalendar('unselect');
       if ($('#calendar').fullCalendar('getView')['name'].indexOf('agenda') != 0) {
@@ -187,6 +352,42 @@ $(document).ready(function() {
         }
         return;
       }
+      $('#dialog-issue').dialogIssue('open', {
+        event: {
+          begin: start,
+          end: end,
+          all_day: allDay,
+        },
+        onClickOk: function(event) {
+          $('#dialog-alert').dialog('option', 'title', 'Info');
+          $('#dialog-alert p').text('Please wait...');
+          $('#dialog-alert').dialog('open');
+
+          $.ajax({
+            async: false,
+            url: '/timesheet/events/create/',
+            type: 'POST',
+            data: {
+              issue: event.issue,
+              begin: event.begin.toISO(),
+              end: event.end.toISO(),
+              all_day: event.all_day,
+            },
+            success: function (data) {
+              $('#dialog-issue').dialogIssue('close');
+              jsonD2F(data);
+              for (var i = 0; i < data.length; i++) {
+                $('#calendar').fullCalendar('renderEvent', data[i]);
+              }
+              $('#dialog-alert').dialog('close');
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+              $('#dialog-alert').dialog('option', 'title', 'Error');
+              $('#dialog-alert p').text('Failure while creating event!');
+            },
+          });
+        },
+      });
     },
   });
 });
